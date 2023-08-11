b0d2d2734ff9f499ea7dbfe97c137d7ba027bbc4
eswarprasadk <eswarprasadk@turito.com>
2023-08-11 21:54:29 +0530

    Server Side rendering

--- server-advanced.js
+++ server-advanced.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 // custom imports
 const friendsRouter = require("./routers/friends.router");
 const app = express();
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 const PORT = 3000;
 
 // middleware
@@ -20,8 +23,19 @@ app.use((req, res, next) => {
   console.log(`request made from IP:${req.ip}`);
   next();
 });
+const static = express.static(path.join(__dirname, "static"));
+app.use("/site", static);
+
+app.get("/template/:name", (req, res) => {
+  const name = req.params.name;
+  res.render("index", { title: `Hare ${name}`, name: `${name}` });
+});
 
 // route handlers or end points
 app.use("/friends", friendsRouter);
+app.get("/hare-krishna", (req, res) => {
+  const pathOfImage = path.join(__dirname, "images", "2219977.jpg");
+  res.sendFile(pathOfImage);
+});
 
 app.listen(PORT, () => console.log(`listening on port no:${PORT}`));
